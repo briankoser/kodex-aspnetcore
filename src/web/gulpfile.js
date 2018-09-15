bdb9461a8d0ce7be329aea6fd1f399a2cf69fe09
Brian Koser <brianmkoser@gmail.com>
2018-09-15 11:05:16 -0500

    Add SCSS

---
+++ src/web/gulpfile.js
@@ -0,0 +1,9 @@
+﻿var gulp = require("gulp")
+var fs = require("fs")
+var sass = require("gulp-sass")
+
+gulp.task("sass", function () {
+    return gulp.src('Styles/site.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('wwwroot/css'));
+});
