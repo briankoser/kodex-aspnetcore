a3f2a7f0f54e043899bafada586e8cfb5dbeaea6
Brian Koser <brianmkoser@gmail.com>
2018-09-15 15:14:01 -0500

    Set up SCSS

--- src/web/gulpfile.js
+++ src/web/gulpfile.js
@@ -1,9 +1,10 @@
-﻿var gulp = require("gulp")
+﻿/// <binding BeforeBuild='sass' />
+var gulp = require("gulp")
 var fs = require("fs")
 var sass = require("gulp-sass")
 
 gulp.task("sass", function () {
-    return gulp.src('Styles/site.scss')
+    return gulp.src('Styles/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('wwwroot/css'));
 });
