ad46146def6cf4f570e93c2036f04692cc5244e5
Brian Koser <brianmkoser@gmail.com>
2018-09-22 11:42:04 -0500

    Add infrastructure project

--- src/application/application/Models/Post.cs
+++ src/application/application/Models/Post.cs
@@ -9,18 +9,19 @@ namespace kodex.Application.Models
     {
         public int ID { get; set; }
         public PostType PostType { get; set; }
-        public IList<Author> Authors { get; set; }
-        public IList<Tag> Tags { get; set; }
         public string Title { get; set; }
         public string Slug { get; set; }
         public string Url { get; set; }
         public string Body { get; set; }
+        public string BodyRaw { get; set; }
         public string Description { get; set; }
-        public DateTime DatePublished { get; set; }
-        public int DatePublishedID { get; set; }
-        public DateTime DateLastUpdated { get; set; }
+        public DateTime? DatePublished { get; set; }
+        public int? DatePublishedID { get; set; }
+        public DateTime? DateLastUpdated { get; set; }
         public string Excerpt { get; set; }
         public string ImageUrl { get; set; }
         public bool IsPublic { get; set; }
+        public IList<Author> Authors { get; set; }
+        public IList<Tag> Tags { get; set; }
     }
 }
