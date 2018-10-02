d29c4f559ce95d630007925e044379bec462d268
Brian Koser <brianmkoser@gmail.com>
2018-10-01 20:37:07 -0500

    Initial posts query

--- src/application/application/Models/Post.cs
+++ src/application/application/Models/Post.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Text;
 
 namespace kodex.Application.Models
@@ -10,17 +12,26 @@ namespace kodex.Application.Models
         public PostType PostType { get; set; }
         public string Title { get; set; }
         public string Slug { get; set; }
-        public string Url { get; set; }
+        public string PostUrl { get; set; }
         public string Body { get; set; }
         public string BodyRaw { get; set; }
         public string Description { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTimeOffset? DatePublished { get; set; }
+
         public int? DatePublishedID { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTimeOffset? DateLastUpdated { get; set; }
+
         public string Excerpt { get; set; }
         public string ImageUrl { get; set; }
         public bool IsPublic { get; set; }
+
         public IList<Author> Authors { get; set; }
         public IList<Tag> Tags { get; set; }
+
+        public string AuthorsString => String.Join(", ", Authors.Select(author => author.FirstName));
     }
 }
--- src/infrastructure/Data/Repositories/PostsRepository.cs
+++ src/infrastructure/Data/Repositories/PostsRepository.cs
@@ -2,12 +2,10 @@
 using kodex.Application.Interfaces;
 using kodex.Application.Models;
 using Microsoft.Extensions.Configuration;
-using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace kodex.Infrastructure.Data.DBAccess
@@ -25,12 +23,14 @@ namespace kodex.Infrastructure.Data.DBAccess
         {
             get
             {
+                // todo: store in secrets.json, etc
                 return new SqlConnection("Server=70.32.29.130;Database=kodexdb;User ID=koserus_kodex;Password=Rd\\_yGRXgke*Ys+mua2s|d1Lg_P|Fm0e;MultipleActiveResultSets=true"); //_configuration.GetConnectionString("KodexDB"));
             }
         }
 
         public async Task<Post> GetByID(int id)
         {
+            //todo: abstract data access library over dapper
             using (IDbConnection conn = Connection)
             {
                 string query = "SELECT * FROM dbo.Posts WHERE ID = @ID";
@@ -48,7 +48,21 @@ namespace kodex.Infrastructure.Data.DBAccess
                                  FROM dbo.Posts 
                                  WHERE (datepublished >= @StartDate OR @StartDate IS NULL)";
                 conn.Open();
+
                 var result = await conn.QueryAsync<Post>(query, new { options.StartDate });
+
+                // todo: get from db
+                var author = new Author() { ID = 1, FirstName = "Brian", FullName = "Brian Koser" };
+                var postType = new PostType() { ID = 1, Code = "post", Name = "Post" };
+                result = result.Select(post =>
+                {
+                    post.Authors = new List<Author>();
+                    post.Authors.Add(author);
+                    post.PostType = postType;
+                    post.PostUrl = $"/{post.PostType.Code}/{post.DatePublished.Value.Year}/{post.DatePublished.Value.Month}/{post.DatePublished.Value.Day}/{post.DatePublishedID}/{post.Slug}";
+                    return post;
+                });
+
                 return result.ToList();
             }
         }
