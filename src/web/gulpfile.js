2111ed21b209057f6c3aec666a0a3d7e64281584
Brian Koser <brianmkoser@gmail.com>
2018-11-07 21:05:33 -0600

    First stab at typography

--- src/web/gulpfile.js
+++ src/web/gulpfile.js
@@ -1,10 +1,19 @@
-﻿/// <binding BeforeBuild='sass' />
-var gulp = require("gulp");
-var fs = require("fs");
-var sass = require("gulp-sass");
+﻿/// <binding BeforeBuild='sass' ProjectOpened='watch-sass' />
+let gulp = require("gulp");
+let fs = require("fs");
+let sass = require("gulp-sass");
+
+let paths = {
+    scss: "Styles/**/*.scss",
+    css: "./wwwroot/css"
+};
 
 gulp.task("sass", function () {
-    return gulp.src('Styles/**/*.scss')
+    return gulp.src(paths.scss)
         .pipe(sass())
-        .pipe(gulp.dest('wwwroot/css'));
+        .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('watch-sass', function () {
+    gulp.watch(paths.scss, ['sass']);
 });
