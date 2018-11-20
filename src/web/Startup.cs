b1ccc15e53ae051af2a1831568a487df1629380a
Brian Koser <brianmkoser@gmail.com>
2018-11-20 15:45:33 -0600

    Update to ASP.NET Core 2.1

--- src/web/Startup.cs
+++ src/web/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using kodex.Application.Interfaces;
 using kodex.Infrastructure.Data.DBAccess;
-using System.Data;
-using System.Data.SqlClient;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Web;
 
 namespace kodex
@@ -28,7 +23,7 @@ namespace kodex
         {
             services.AddTransient<IPostsRepository, PostsRepository>();
             services.AddTransient<ISqlDataSourceConfig, SqlDataSourceConfig>();
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,14 +31,15 @@ namespace kodex
         {
             if (env.IsDevelopment())
             {
-                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
             else
             {
                 app.UseExceptionHandler("/Error");
+                app.UseHsts();
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseMvc();
