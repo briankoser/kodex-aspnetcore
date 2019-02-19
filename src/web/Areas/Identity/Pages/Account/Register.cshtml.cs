f4e25e9404a4a0598cc00491e61f9c68bb760f70
Brian Koser <brianmkoser@gmail.com>
2019-02-19 11:43:25 -0600

    Add name field to registration

--- src/web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -42,6 +42,9 @@ namespace Web.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
 
+            [Display(Name = "Name")]
+            public string Name { get; set; }
+
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
@@ -64,7 +67,8 @@ namespace Web.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = Input.Email, Email = Input.Email };
+                string userName = String.IsNullOrEmpty(Input.Name) ? Input.Email : Input.Name;
+                var user = new User { UserName = userName, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
